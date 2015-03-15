9d7e3c3808bb55b3f36987fec067796e6eb3c401
Patrik Lindstrom <patriklindstrom@hotmail.com>
2015-03-15 21:09:48 +0100

    First baseline
    included Nunit Test and BouncyCastle CommandLineParser Nuget. To fix
    encryption.  Gist from where core is

---
+++ EnCustData.Test/StringEncryptionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EnCustData.Test
+{[TestFixture]
+    public class StringEncryptionTest
+    {
+    [Test]
+    public void EncryptAndDecryptAString( )
+    {
+        //Arrange
+        string passPhrase = "Ni_talar_bra_latin";
+        // http://www.hollyfame.com/top-10-greatest-kept-hollywood-secrets-of-all-time/10/
+        string secretString = "Most Pop Stars Lip-sync Onstage";
+
+        //Act
+
+        //Assert
+
+    }
+    }
+}
