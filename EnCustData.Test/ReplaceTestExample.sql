6adeda3254873ea20b946d78372e427d83e7d3e9
Patrik Lindström <patrik.lindstrom@lcube.se>
2015-03-16 17:04:25 +0100

    Adding sql script
    For creating sql table EncryptVal_TB and examples of sql that should be
    run todo a replace.

---
+++ EnCustData.Test/ReplaceTestExample.sql
@@ -0,0 +1,15 @@
+﻿-- 1. Remove all FAX-numbers, email, etc. from organizations
+begin transaction
+select * from clo_OrganizationContact_TB
+update clo_OrganizationContact_TB
+set ContactName = CAST(NEWID() as nvarchar(36))
+select * from clo_OrganizationContact_TB
+commit transaction
+
+-- 2. Remove all FAX-numbers, email, etc. from holders
+begin transaction
+select * from clo_HolderContact_TB
+update clo_HolderContact_TB
+set ContactName = CAST(NEWID() as nvarchar(36))
+select * from clo_HolderContact_TB
+commit transaction
